3a20e2f837758542cbe3afaccc8380ac241fabb6
muathalzoubi95 <moathaza@gmail.com>
2025-11-26 06:14:58 +0300

    asdas

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,3 +3,4 @@ import { createBrowserClient } from "@supabase/ssr"
 export function createClient() {
   return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 }
+createClient
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,20 +1,25 @@
-import { createServerClient } from "@supabase/ssr"
-import { cookies } from "next/headers"
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
 
-export async function createClient() {
-  const cookieStore = await cookies()
+export function createClient() {
+  const cookieStore = cookies()
 
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll()
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
+          } catch {}
+        },
       },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-        } catch {
-        }
-      },
-    },
-  })
+    }
+  );
 }
